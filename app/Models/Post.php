bc40dc8272e8a88be6d82f4bd18f26ede37704db
youssefreda4 <154443732+youssefreda4@users.noreply.github.com>
2025-02-24 23:14:28 +0200

    use Storage::url

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
 {
@@ -48,7 +49,7 @@ class Post extends Model
     public function image()
     {
         if ($this->image) {
-            return asset($this->image);
+            return Storage::url($this->image);
         }
         return asset('default.png');
     }
