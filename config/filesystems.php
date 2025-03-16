418633f3d78f99ce38fde671ae170864537d87db
youssefreda4 <154443732+youssefreda4@users.noreply.github.com>
2025-03-17 01:04:05 +0200

    edit url in public in filesystems

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/',
+            'url' => env('APP_URL').'/public',
             'visibility' => 'public',
             'throw' => false,
         ],
